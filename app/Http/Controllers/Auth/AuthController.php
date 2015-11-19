b50cd510b6e61a49e1a199ecd978d36ddc04c3ff
Chris Viccaro <cviccaro@jpenterprises.com>
2015-11-19 15:19:01 -0500

    Initial commit and setup.

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Exception\HttpResponseException;
+use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+/**
+ * @link https://github.com/tymondesigns/jwt-auth/wiki/Creating-Tokens
+ */
+
+class AuthController extends Controller {
+
+	public function authenticate(Request $request) {
+
+		$credentials = $request->only('name', 'password');
+
+		try {
+			if (! $token = JWTAuth::attempt($credentials)) {
+				return response()->json(['error' => 'invalid_credentials'], 401);
+			}
+		}
+		catch (JWTException $e) {
+			return response()->json(['error' => 'could_not_create_token'], 500);
+		}
+
+		return response()->json(compact('token'));
+	}
+
+	public function refresh(Request $request){
+
+	     $oldtoken = JWTAuth::getToken();
+
+	     try {
+	         // attempt to refresh token for the user
+	         if (! $token = JWTAuth::parseToken('bearer','authorization', $oldtoken)->refresh()) {
+	             return response()>json(['error' => 'invalid_token'], 401);
+	         }
+	     } 
+	     catch (JWTException $e) {
+	         return response()->json(['error' => 'could_not_refresh_token'], 500);
+	     }
+
+	     return response()->json(compact('token'));
+	 }
+}
---
+++ bootstrap/app.php
@@ -0,0 +1,109 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+Dotenv::makeMutable();
+Dotenv::load(__DIR__.'/../');
+Dotenv::makeImmutable();
+
+/*
+|--------------------------------------------------------------------------
+| Create The Application
+|--------------------------------------------------------------------------
+|
+| Here we will load the environment and create the application instance
+| that serves as the central piece of this framework. We'll use this
+| application as an "IoC" container and router for this framework.
+|
+*/
+
+$app = new Laravel\Lumen\Application(
+    realpath(__DIR__.'/../')
+);
+
+$app->withFacades();
+$app->withEloquent();
+
+class_alias('Illuminate\Support\Facades\Response', 'Response');
+
+$app->configure('jwt');
+class_alias('Tymon\JWTAuth\Facades\JWTAuth', 'JWTAuth');
+class_alias('Tymon\JWTAuth\Facades\JWTFactory', 'JWTFactory');
+
+/*
+|--------------------------------------------------------------------------
+| Register Container Bindings
+|--------------------------------------------------------------------------
+|
+| Now we will register a few bindings in the service container. We will
+| register the exception handler and the console kernel. You may add
+| your own bindings here if you like or you can make another file.
+|
+*/
+
+$app->singleton(
+    Illuminate\Contracts\Debug\ExceptionHandler::class,
+    App\Exceptions\Handler::class
+);
+
+$app->singleton(
+    Illuminate\Contracts\Console\Kernel::class,
+    App\Console\Kernel::class
+);
+
+/*
+|--------------------------------------------------------------------------
+| Register Middleware
+|--------------------------------------------------------------------------
+|
+| Next, we will register the middleware with the application. These can
+| be global middleware that run before and after each request into a
+| route or middleware that'll be assigned to some specific routes.
+|
+*/
+
+$app->middleware([
+    Illuminate\Cookie\Middleware\EncryptCookies::class,
+    Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+    Illuminate\Session\Middleware\StartSession::class,
+    Illuminate\View\Middleware\ShareErrorsFromSession::class,
+]);
+
+ $app->routeMiddleware([
+    'jwt.auth'    => Tymon\JWTAuth\Middleware\GetUserFromToken::class,
+    'jwt.refresh' => Tymon\JWTAuth\Middleware\RefreshToken::class,
+ ]);
+
+/*
+|--------------------------------------------------------------------------
+| Register Service Providers
+|--------------------------------------------------------------------------
+|
+| Here we will register all of the application's service providers which
+| are used to bind services into the container. Service providers are
+| totally optional, so you are not required to uncomment this line.
+|
+*/
+
+$app->register('Tymon\JWTAuth\Providers\JWTAuthServiceProvider');
+
+//Development
+$app->register('Conner\Kint\KintServiceProvider');
+$app->register('Vluzrmos\Tinker\TinkerServiceProvider');
+
+/*
+|--------------------------------------------------------------------------
+| Load The Application Routes
+|--------------------------------------------------------------------------
+|
+| Next we will include the routes file so that they can all be added to
+| the application. This will provide all of the URLs the application
+| can respond to, as well as the controllers that may handle them.
+|
+*/
+
+$app->group(['namespace' => 'App\Http\Controllers'], function ($app) {
+    require __DIR__.'/../app/Http/routes.php';
+});
+
+return $app;
