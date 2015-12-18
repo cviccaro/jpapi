f317d574c4a9d838188a5a50f8bccd689457cae5
Chris Viccaro <cviccaro@jpenterprises.com>
2015-12-18 08:42:52 -0500

    Added users to UserTableSeeder

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -15,9 +15,24 @@ class UserTableSeeder extends Seeder
         DB::table('users')->delete();
 
         User::create([
-        	'name' => 'jpapi_admin',
-        	'email' => 'cviccaro@jpenterprises.com',
-        	'password' => Hash::make('ipapj')
-    	]);
+            'name' => 'jpapi_admin',
+            'email' => 'cviccaro@jpenterprises.com',
+            'password' => Hash::make('ipapj')
+        ]);
+        User::create([
+            'name' => 'sgebhart',
+            'email' => 'sgebhart@jpenterprises.com',
+            'password' => Hash::make('3Lu3d4T2')
+        ]);
+        User::create([
+            'name' => 'jmaiers',
+            'email' => 'jmaiers@jpenterprises.com',
+            'password' => Hash::make('YE8yshwX')
+        ]);
+        User::create([
+            'name' => 'pgarvey',
+            'email' => 'pgarvey@jpenterprises.com',
+            'password' => Hash::make('EYnHRc93')
+        ]);
     }
 }
